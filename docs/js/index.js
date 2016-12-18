3454ae7750ce0499830b91d029927aad62161278
Rawnly <fedevitale99@gmail.com>
2016-12-18 02:59:48 +0100

    Too many things

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,13 +1,26 @@
 $(document).ready(function() {
-    // $('meta[name="theme-color"]').attr('content', $('body').css('background-color'));
-    // $(window.screen).change(function () {
-    //   $('meta[name="theme-color"]').attr('content', $('body').css('background-color'));
-    // });
-
     $('a').click(function() {
         $('html, body').animate({
             scrollTop: $('[id="' + $.attr(this, 'href').substr(1) + '"]').offset().top
         }, 900);
         return false;
     });
+
+    var bg = new Image();
+
+    bg.onload = function(){
+        $("#page1").css("background-image", "url(" + bg.src + ")");
+    }
+
+    bg.src = "../css/res/wp.jpg";
 });
+
+(function( $ ) {
+  $.fn.isVisible = function () {
+    if ( this.is(':visible') ) {
+      return true
+    } else {
+      return false
+    }
+  }
+} ( jQuery ) );
